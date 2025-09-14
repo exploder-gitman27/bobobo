27f56e11527d4ab002a7605cfe903c645067e3b2
exploder-gitman27 <explodergit@outlook.jp>
2025-09-14 10:32:28 +0000

    レビューテスト

---
+++ test.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class RandomSample2 {
+ 
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(11);
+        System.out.println(randomNumber);
+    }
+
+}
+
+public class RandomSample2 {
+ 
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(11);
+        System.out.println(randomNumber);
+    }
+
+}
